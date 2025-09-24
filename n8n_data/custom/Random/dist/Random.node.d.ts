348d7a955b10acbbf05bbeb80843edf4e84df1fb
kellybarbosaw <kelly.barbosa@aldiweb.com.br>
2025-09-23 21:24:29 -0300

    removendo arquivos desnecessários adicionando o .gitignore

--- n8n_data/custom/Random/dist/Random.node.d.ts
+++
@@ -1,6 +0,0 @@
-import type { IExecuteFunctions } from 'n8n-workflow';
-import type { INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
-export declare class Random implements INodeType {
-    description: INodeTypeDescription;
-    execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
-}
